3776972ab4e2a5b12552b22dfe6d1906b8d1a9de
alpoxdev <alpoxdev@gmail.com>
2021-05-31 18:17:09 +0900

    done: guessing-game

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,18 +1,40 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 // https://doc.rust-lang.org/book/ch02-00-guessing-game-tutorial.html
 
 fn main() {
     println!("Guess The Number!");
-
     println!("Please Input your guess!");
 
-    let mut guess = String::new(); // mutable variable
-    let immutable = "immutable"; // immutable variable
+    let secret_number = rand::thread_rng().gen_range(1..101);
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line!");
+    loop {
+        let mut guess = String::new(); // mutable variable
+        let immutable = "immutable"; // immutable variable
 
-    println!("You guessed : {}", guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("{}", err);
+                continue;
+            }
+        };
+
+        println!("You guessed : {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Less!"),
+            Ordering::Greater => println!("Greater!"),
+            Ordering::Equal => {
+                println!("Equal!");
+                break;
+            }
+        }
+    }
 }
