40f9821e0f409f310683e2ab39579defa51dfd83
alpoxdev <alpoxdev@gmail.com>
2021-05-31 18:38:04 +0900

    add: 3.1 variables and mutablity

---
+++ project3/target/debug/deps/project3.d
@@ -0,0 +1,5 @@
+/Users/alpox/Desktop/dev/rust/project3/target/debug/deps/project3: src/main.rs
+
+/Users/alpox/Desktop/dev/rust/project3/target/debug/deps/project3.d: src/main.rs
+
+src/main.rs:
---
+++ project3/target/debug/project3.d
@@ -0,0 +1,1 @@
+/Users/alpox/Desktop/dev/rust/project3/target/debug/project3: /Users/alpox/Desktop/dev/rust/project3/src/main.rs
