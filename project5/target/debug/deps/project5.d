3491de457f47c21aca62c4a72f5af834225ca5dc
alpoxdev <alpoxdev@gmail.com>
2021-06-06 03:17:05 +0900

    add: project5

--- project5/target/debug/deps/project5.d
+++
@@ -1,5 +0,0 @@
-/Users/alpox/Desktop/dev/rust/project5/target/debug/deps/project5: src/main.rs
-
-/Users/alpox/Desktop/dev/rust/project5/target/debug/deps/project5.d: src/main.rs
-
-src/main.rs:
--- project5/target/debug/project5.d
+++
@@ -1,1 +0,0 @@
-/Users/alpox/Desktop/dev/rust/project5/target/debug/project5: /Users/alpox/Desktop/dev/rust/project5/src/main.rs
