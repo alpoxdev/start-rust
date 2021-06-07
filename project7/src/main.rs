8100fbfb4ba8151ca26a907efb477ea3b0feadb0
alpoxdev <alpoxdev@gmail.com>
2021-06-07 21:48:32 +0900

    fix: project7 mod

--- project7/src/main.rs
+++ project7/src/main.rs
@@ -1,4 +1,24 @@
-// 7.1 Packages and Crates
-fn main() {
-    println!("Hello, world!");
+// extern crate communicator;
+extern crate project8;
+
+pub mod a {
+	pub mod series {
+			pub mod of {
+					pub fn nested_modules(x: String) {
+						println!("{}", x);
+					}
+			}
+	}
+}
+
+use a::series::of::nested_modules;
+use project8::*;
+
+fn main(){
+	client::connect();
+	// communicator::network::connect();
+	// communicator::network::server::connect();
+
+	a::series::of::nested_modules(String::from("Hello, World!"));
+	nested_modules(String::from("Use Use!"));
 }
